ff56739c9ee164788ef78ea94d00b46f0628140f
williamminty <william.minty@stqry.com>
2020-04-30 10:39:03 +1200

    first commit

---
+++ varieties.php
@@ -0,0 +1,219 @@
+<?php
+$title = "Varieties";
+$description ="Description of the Varieties page";
+require("templates/header.php");
+
+?>
+
+
+<div class="row mt-5">
+
+  <div class="col-6 mb-2 card">
+    <?php
+      $src="images/Coffee/ice-cream-iced-coffee with-whipped-cream.jpg";
+      $coffeeName="Ice cream iced coffee with whipped cream";
+      $instructions="
+      DESCRIPTION
+        What is a café Americano? It is made by
+        adding hot water to an espresso shot.
+        The water dilutes the espresso a bit,
+        giving you the volume of a drip,
+        but with an espresso taste. Read on to
+        learn how to make the perfect tasting
+        Americano yourself!
+        SCALE
+        1x
+        2x
+        3x
+      INGREDIENTS
+        14–18 grams espresso coffee beans
+        hot or almost boiling water
+        espresso machine
+        scale
+        grinder
+        tamper
+      INSTRUCTIONS
+        Measure out your beans for a double shot.
+        Grind them really fine.
+        Tamp the beans, then put the portafilter
+        into its spot on the machine.
+        Make the espresso.
+        Heat up the water to 160-170 degrees Fahrenheit.
+        Pour the espresso into the hot water, mix 1
+        part espresso to 2 parts water.
+      ";
+
+      $link="https://www.homegrounds.co/how-to-make-an-americano/";
+      require("templates/card.php");
+    ?>
+  </div>
+
+
+  <div class="col-6 mb-2 card">
+    <?php
+      $src="images/Coffee/cafe-au-lait.jpg";
+      $coffeeName="Cafe Au Lait";
+      $instructions="
+      In France, equal parts of steamed milk and
+      strong hot coffee are combined to make
+      the perfect mellow beverage known as a café au lait.
+      Prepare this easy recipe at home and serve it at a
+      traditional French breakfast
+      with flaky croissants or as an after-brunch coffee
+      with a single square of dark chocolate.
+      Your guests will think they've been transported
+      to a sidewalk café in Paris.
+      This recipe makes one serving, but the proportions
+      can be increased for multiple
+      cups of this gentle pick-me-up.
+      In Spain, this identical drink is known as cafe con leche,
+      while Germans call it milchkaffee.
+      Vegans need not feel left out of this cafe au lait recipe.
+      For those who like to eat their coffee for dessert,
+      don't miss this café au lait creme brulée.
+      Ingredients
+      1 part hot strong coffee (French roast)
+      1 part steamed milk
+      ";
+      $link="https://www.thespruceeats.com/cafe-au-lait-recipe-1374920";
+      require("templates/card.php");
+    ?>
+  </div>
+
+
+  <div class="col-6 mb-2 card">
+    <?php
+      $src="images/Coffee/irish_coffee.jpg";
+      $coffeeName="Irish Coffee";
+      $instructions="How to Make the Best Irish Coffee
+        Just start with the best ingredients!
+        1) Brew your favorite coffee. I like dark roast
+        best in Irish coffees,
+        personally, but that’s my general preference.
+        Freshly brewed coffee is the way to go!
+        2) Add Irish whiskey to a mug. I’m partial to Jameson.
+        It’s the brand we used to
+        make Irish coffees when I was a bartender. Bushmills
+        is a less expensive option.
+        3) Add a little maple syrup to sweeten. Mind you,
+        I like my coffee black in the mornings,
+        but a little sweetener takes the edge off the whiskey.
+        You could use regular sugar
+        or brown sugar instead, but maple syrup tastes better
+        and blends in more easily.
+        4) Add a splash of coffee and gently stir to blend.
+        Then fill the mug with coffee,
+        leaving about 1/2″ at the top for whipped cream.
+        5) Top with whipped cream. Bonus points if you’re 
+        using real whipped cream. So good!";
+
+        $link="https://cookieandkate.com/best-irish-coffee-recipe/";
+      require("templates/card.php");
+
+    ?>
+  </div>
+
+  <div class="col-6 mb-2 card">
+    <?php
+      $src="images/kahula-coffee-soda.jpg";
+      $coffeeName="Kahula Coffee Soda";
+      $instructions="INGREDIENTS
+      1 shot of espresso
+      1½ ounces Kahlua coffee flavored liqueur
+      2-4 dashes black walnut or aromatic bitters
+      ice
+      soda water
+      half & half
+      
+      INSTRUCTIONS
+      Fill a tall glass with ice, pour over a shot of espresso 
+      followed by the Kahlua, add a few dashes of bitters. Top 
+      with soda water and half & half. Sip through a straw while 
+      lounging.
+      NOTES
+      You could sub the Kahlua for Bailey's or even punch this 
+      cocktail up with the addition of vodka, whiskey, or rum! 
+      Alternatively, leave out the alcohol for a delicious and 
+      refreshing take on iced coffee.";
+
+        $link="https://www.brighteyedbaker.com/diy-peppermint-white-chocolate-mocha//";
+      require("templates/card.php");
+
+    ?>
+  </div>
+
+  <div class="col-6 mb-2 card">
+    <?php
+      $src="images/Coffee/white-chocolate-mocha-peppermint.jpg";
+      $coffeeName="White-Chocolate-Peppermint-Mocha";
+      $instructions="Combine cream and sugar in a saucepan over medium heat and bring just to a boil. Meanwhile, place white chocolate in a microwave-safe bowl.
+      Pour hot cream mixture over chocolate and let sit for 5 minutes. Whisk until smooth, microwaving for about 10 seconds if needed to ensure all chocolate is completely melted. Whisk in the peppermint extract.
+      Strain through a fine-mesh sieve into a jar or other airtight container. Cool at room temperature, and then transfer to refrigerator to store.
+      PEPPERMINT WHITE CHOCOLATE MOCHA
+      Heat milk and syrup until steaming and whisk to froth, or use a milk frother to heat and froth.
+      Transfer milk mixture to coffee cup and pour coffee on top. Stir together. Top with whipped cream and peppermint candy, if desired.
+      notes". "<br>" . "Ingrediants" . "<br>" . "PEPPERMINT WHITE CHOCOLATE SYRUP*
+      8 1/4 ounces (1 cup) heavy cream
+      3 1/2 ounces (1/2 cup) granulated sugar
+      4 ounces white chocolate, finely chopped
+      1/4 teaspoon peppermint extract
+      PEPPERMINT WHITE CHOCOLATE MOCHA
+      4 1/4 ounces (1/2 cup) milk**
+      2 tablespoons peppermint white chocolate syrup***
+      double shot of espresso (or about 5 ounces strongly-brewed coffee)
+      whipped cream, for topping, optional****
+      chopped/crushed peppermint candy, for sprinkling, optional";
+
+        $link="https://www.brighteyedbaker.com/diy-peppermint-white-chocolate-mocha//";
+      require("templates/card.php");
+
+    ?>
+  </div>
+
+  <div class="col-6 mb-2 card">
+    <?php
+      $src="images/Coffee/rose-lavender-honey-iced-lattes.jpg";
+      $coffeeName="rose-lavender-honey-iced-lattes";
+      $instructions="Ingredients
+      For the rose lavender honey simple syrup
+      3/4 cup water
+      3/4 cup honey
+      1/4 cup dried culinary lavender*
+      1/2 cup dried rosebuds*
+      For the latte
+      1 tablespoon rose lavender honey simple syrup
+      1 shot of espresso
+      1/2 cup whole milk*
+      crushed ice
+      fresh rose petals or lavender for garnish, optional
+      
+      Instructions
+      rose lavender simple syrup
+      In a medium saucepan, bring the water, honey, lavender, and 
+      rosebuds to a boil. Once boiling, stir for 10 seconds, remove
+      from heat, and stir until honey is dissolved. Let cool 
+      completely. Once cooled, strain mixture through a fine mesh 
+      strainer into a container and discard lavender and rosebuds. 
+      Syrup will keep in a sealed container in the fridge for up to 
+      2 weeks.
+
+      latte
+      Add the simple syrup to a glass. Fill glass with crushed ice. 
+      Pour the espresso shot over the ice, and top with milk. Stir 
+      really well to incorporate the syrup into the drink. Top with 
+      a fresh rose petal and/or a sprig of lavender if desired. Enjoy!";
+
+        $link="https://www.brighteyedbaker.com/diy-peppermint-white-chocolate-mocha//";
+      require("templates/card.php");
+
+    ?>
+  </div>
+
+</div>
+
+
+<?php
+require("templates/footer.php");
+
+?>
+
